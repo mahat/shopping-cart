40a52e5ab4dfc94a4f249ca3e5d40295c6adfc84
mahat <mehmet.ahat@gmail.com>
2017-09-11 23:50:24 +0300

    end of part 6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping-Cart' });
+  Product.find(function (err, docs) {
+      if (err != undefined) {
+        console.log('error getting data from DB');
+      }
+
+      var productChunks = [];
+      var chunkSize = 3;
+      for (var i =0 ;i < docs.length; i+=chunkSize) {
+        productChunks.push(docs.slice(i,i + chunkSize));
+      }
+
+      res.render('shop/index', { title: 'Shopping-Cart', products: productChunks});
+  });
+});
+
+router.get('/user/signup', function(req,res) {
+  //console.log( req.csrfToken());
+  res.render('user/signup',{csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', function(req, res, next){
+  res.redirect('/');
 });
 
 module.exports = router;
