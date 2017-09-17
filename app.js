b24db45a727a64cfd4f6e2ffb8377e96d0c66ff0
mahat <mehmet.ahat@gmail.com>
2017-09-17 22:55:30 +0300

    end of part 13

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var session = require('express-session')
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
+
 var index = require('./routes/index');
 var user = require('./routes/user');
 
@@ -34,8 +36,12 @@ app.use(cookieParser());
 app.use(session({
   secret: 'mysupersecret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection}),
+  // setting timeout for cookies
+  cookie: { maxAge: 10 * 60 * 1000 }
 }));
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
